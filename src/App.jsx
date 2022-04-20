fa16ed640bcccd27117a1d6957905784c046d378
Misha Novikov <novikov1807@gmail.com>
2022-04-20 13:05:53 +0300

    full.2.4

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<TableRegion />} />
         <Route path="comment" element={<TableComment />} />
-
+        <Route exact path="/" element={<TableRegion />} />
       </Routes>
     </div>
   );
